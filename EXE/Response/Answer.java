5e10fcdaf340946d4f28e91c4e5ca5ba96cad899
pol_gay <pol.gay@estudiantat.upc.edu>
2025-10-25 19:06:00 +0200

    he canviat carpeta de reponse

--- EXE/Response/Answer.java
+++ EXE/Response/Answer.java
@@ -1,1 +1,76 @@
-package
+package response; 
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public abstract class Answer {
+    private int questionId; 
+
+
+    public Answer(int questionId) {
+        if (questionId < 0) {
+            throw new IllegalArgumentException("id no pot ser negatiu"); 
+        }
+        this.questionId = questionId; 
+
+    }
+
+    //getters
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    //tipus de resposta 
+    public abstract Type getType();
+    public abstract boolean isEmpty();
+    public enum Type {
+        TEXT, 
+        INT, 
+        MULTIPLE_CHOISE, 
+        SINGLE_CHOISE
+    } 
+
+
+
+    //if it is text 
+    public static TextAnswer text(int questionId, String value) {
+        return new TextAnswer(questionId, value);
+    }
+    //if it is an integer
+    public static IntAnswer integer(int questionId, int value) {
+        return new IntAnswer(questionId, value);
+    }
+
+
+    //if it is multiple choice, options separated by commas
+    public static MultipleChoiceAnswer multipleChoise(int questionId, String optionIdsCsv) {
+        return new MultipleChoiceAnswer(questionId, optionIdsCsv);
+    }
+ //if it is multiple choice, and collection of integers options 
+
+    public static MultipleChoiceAnswer multipleChoise(int questionId, Collection<Integer> optionIds, Set<Integer> validOptionIds) {
+        return new MultipleChoiceAnswer(questionId, optionIds, validOptionIds);
+    }
+
+
+    //if it is single choice
+    public static SingleChoiceAnswer singleChoise(int questionId, int optionId, Set<Integer> validOptionIds) {
+        return new SingleChoiceAnswer(questionId, optionId, validOptionIds);
+    }
+
+    public static EmptyAnswer empty(int questionId) {
+        return new EmptyAnswer(questionId);
+    }
+   
+
+
+
+
+
+}
+
+
+
---
+++ EXE/Response/TextAnswer.java
@@ -0,0 +1,40 @@
+package response; 
+
+
+public final class TextAnswer extends Answer {
+    private final String value; 
+
+    public TextAnswer(int questionId, String value) {
+        super(questionId); 
+        //no pot ser null 
+        if (value == null) {
+            throw new IllegalArgumentException("El valor no pot ser null");
+        }
+
+        this.value = value; 
+    }
+
+
+    //gets the value 
+    public String getValue() {
+        return value; 
+    }
+
+    //returns the type of the answer
+    @Override
+    public Type getType() {
+        return Type.TEXT;
+    }
+
+    //returns if the answer is empty
+    @Override
+    public boolean isEmpty() {
+        return value == null || value.trim().isEmpty();
+    }
+
+    //string representation of the object
+    @Override 
+    public String toString() { 
+        return "TextAnswer{q=" + getQuestionId() + ", value=\"" + value + "\"}"; 
+    }
+}
